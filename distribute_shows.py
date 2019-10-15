5058fd1bf60548eaaf918725271ce55a606997da
Tim Gallagher <tspgallagher@gmail.com>
2019-10-15 03:13:17 -0500

    Create for loop to parse both complete and Torrents directories and put files in proper directories

--- distribute_shows.py
+++ distribute_shows.py
@@ -19,45 +19,52 @@ def find_show_name(episode_name: str) -> str:
     return str(show_name)
 
 
-complete_dir = "/Users/tspgallagher/Projects/Python/TV Show Parser/complete"
-torrents_dir = "/Users/tspgallagher/Projects/Python/TV Show Parser/Torrents"
-tv_dowloads_root = "/Users/tspgallagher/Projects/Python/TV Show Parser/TVDownloads"
+def deposit_files(path: Path):
+    for p in path.iterdir():
+        if p.is_dir():
+            p.cwd()
+            dld_episode_dir = Path(p)
+            for dep in dld_episode_dir.iterdir():
+                if (dep.suffix == ".mkv"):
+                    src_name = str(dep)
+                    # Throw away any obfuscation
+                    episode_name = dep.parts[-2]
+                    show_name = find_show_name(episode_name)
+                    # print("show_name = " + show_name)
+                    dst_directory = Path(tv_dowloads_root + sep + show_name)
+                    if not dst_directory.exists():
+                        try:
+                            os.mkdir(dst_directory)
+                        except:
+                            print(
+                                "Unable to make destination directory" + dst_directory)
+                            exit
+
+                    dst_name = str(dst_directory) + sep + \
+                        episode_name + dep.suffix
+                    try:
+                        shutil.copy(src_name, dst_name)
+                    except (shutil.Error, OSError, IOError):
+                        print("Could not move file \n\"" +
+                              src_name + "\"\nto\n\"" + dst_name + "\"")
+                    else:
+                        pass
+                        # shutil.rmtree(dld_episode_dir)
+
+
+complete_dir = Path(
+    "/Users/tspgallagher/Projects/Python/TV-Show-Parser/complete")
+torrents_dir = Path(
+    "/Users/tspgallagher/Projects/Python/TV-Show-Parser/Torrents")
+tv_dowloads_root = "/Users/tspgallagher/Projects/Python/TV-Show-Parser/TVDownloads"
 sep = "/"
 
-path = Path(complete_dir)
-# path = Path(torrents_dir)
 tvd_path = Path(tv_dowloads_root)
 
 if not(tvd_path.is_dir() and tvd_path.exists):
     tvd_path.mkdir()
 
-if not(path.is_dir() and path.exists):
-    exit
-
-for p in path.iterdir():
-    if p.is_dir():
-        p.cwd()
-        dld_episode_dir = Path(p)
-        for dep in dld_episode_dir.iterdir():
-            if (dep.suffix == ".mkv"):
-                src_name = str(dep)
-                episode_name = dep.parts[-2]    # Throw away any obfuscation
-                show_name = find_show_name(episode_name)
-                # print("show_name = " + show_name)
-                dst_directory = Path(tv_dowloads_root + sep + show_name)
-                if not dst_directory.exists():
-                    try:
-                        os.mkdir(dst_directory)
-                    except:
-                        print("Unable to make destination directory" + dst_directory)
-                        exit
-
-                dst_name = str(dst_directory) + sep + episode_name + dep.suffix
-                try:
-                    shutil.copy(src_name, dst_name)
-                except (shutil.Error, OSError, IOError):
-                    print("Could not move file \n\"" +
-                          src_name + "\"\nto\n\"" + dst_name + "\"")
-                else:
-                    pass
-                    # shutil.rmtree(dld_episode_dir)
+for path in [complete_dir, torrents_dir]:
+    if not(path.is_dir() and path.exists):
+        exit
+    deposit_files(path)
