5449475fe3c1e35ce2eb0a5214cf64c67a482736
Tim Gallagher <tspgallagher@gmail.com>
2019-10-18 08:46:26 -0500

    Added copy_file method to try to improve performance

--- distribute_shows.py
+++ distribute_shows.py
@@ -4,7 +4,52 @@ import re
 import os
 
 
+def copy_file(src, dst, buffer_size=10485760, preserve_file_date=True):
+    """
+    Copies a file to a new location.
+    Much faster performance than Apache Commons due to use of larger buffer.
+
+    :param src:    Source file path
+    :param dst:    Destination file path
+    :param buffer_size:    Buffer size to use during copy
+    :param preserve_file_date:    Preserve the original file date
+    """
+
+    # Optimize the buffer for small files
+    buffer_size = min(buffer_size, os.path.getsize(src))
+    if buffer_size == 0:
+        buffer_size = 1024
+
+    if shutil._samefile(src, dst):
+        raise shutil.Error(
+            "`{0}` and `{1}` are the same file".format(src, dst))
+    for fn in [src, dst]:
+        try:
+            st = os.stat(fn)
+        except OSError:  # File most likely does not exist
+            pass
+        else:  # XXX What about other special files? (sockets, devices...)
+            if shutil.stat.S_ISFIFO(st.st_mode):
+                raise shutil.SpecialFileError(
+                    "`{}` is a named pipe".format(fn))
+    with open(src, 'rb') as fsrc:
+        with open(dst, 'wb') as fdst:
+            shutil.copyfileobj(fsrc, fdst, buffer_size)
+
+    if preserve_file_date:
+        shutil.copystat(src, dst)
+
+
 def find_show_name(episode_name: str) -> str:
+    """
+    Sucks out show_name from episode_name
+    So if episode_name was "Blue.Bloods.S10E03.1080p.WEB.H264-AMCON[rarbg]" then
+    show_name would result in "Blue Bloods"
+    The show "S.W.A.T." is a special case
+
+    :param  episode_name:   String containing episode name pulled from source directory
+    """
+
     idx = re.search("([sS][0-9][0-9][eE][0-9][0-9])", episode_name).start()
     show_name = episode_name[:idx-1]
     try:
@@ -43,7 +88,9 @@ def deposit_files(path: Path):
                     dst_name = str(dst_directory) + sep + \
                         episode_name + dep.suffix
                     try:
-                        shutil.copy(src_name, dst_name)
+                        #    shutil.copy(src_name, dst_name)
+                    copy_file(src_name, dst_name)
+
                     except (shutil.Error, OSError, IOError):
                         print("Could not move file \n\"" +
                               src_name + "\"\nto\n\"" + dst_name + "\"")
